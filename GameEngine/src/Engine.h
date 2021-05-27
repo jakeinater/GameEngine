a25a299a448b51b50fe86dfbb9cfbb4fe6b10e1e
Jake Zhao <zhaojake14@gmail.com>
2021-05-26 22:36:58 -0700

    spd logging wrapper

--- GameEngine/src/Engine.h
+++ GameEngine/src/Engine.h
@@ -3,6 +3,7 @@
 //for use by applications
 
 #include "Engine/Application.h"
+#include "Engine/Log.h"
 
 //--------Entry Point-----------------//
 #include "Engine/EntryPoint.h"
--- GameEngine/src/Engine/EntryPoint.h
+++ GameEngine/src/Engine/EntryPoint.h
@@ -6,6 +6,11 @@ extern Engine::Application* Engine::CreateApplication();
 
 int main(int argc, char** argv)
 {
+	Engine::Log::Init();
+	ME_CORE_WARN("Initialized log!");
+	int a = 5;
+	ME_INFO("Initialized log! Var={0}", a);
+
 	auto app = Engine::CreateApplication();
 	app->Run();
 	delete app;
