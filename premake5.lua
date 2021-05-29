bf3fe980ca9a18e782b6319f7c10356bc67f9efc
Jake Zhao <zhaojake14@gmail.com>
2021-05-28 23:46:07 -0700

    logging, premake

---
+++ premake5.lua
@@ -0,0 +1,104 @@
+
+workspace "GameEngine"
+	architecture "x64"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "GameEngine"
+	location "GameEngine"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"%{prj.name}/vendors/spdlog/include"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"ME_PLATFORM_WINDOWS",
+			"ME_BUILD_DLL"
+		}
+
+		postbuildcommands
+		{
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+		}
+
+	filter "configurations:Debug"
+		defines "ME_DEBUG"
+		symbols "On"
+	
+	filter "configurations:Release"
+		defines "ME_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "ME_DIST"
+		optimize "On"
+
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"GameEngine/src"
+	}
+
+	links "GameEngine"
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"ME_PLATFORM_WINDOWS",
+		}
+
+	filter "configurations:Debug"
+		defines "ME_DEBUG"
+		symbols "On"
+	
+	filter "configurations:Release"
+		defines "ME_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "ME_DIST"
+		optimize "On"
+
+
